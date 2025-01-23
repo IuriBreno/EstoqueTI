920d4d1d322dc2cefe2d12e0dbf11724b00d74a9
IuriBreno <brenoiuri42@gmail.com>
2025-01-23 19:32:56 -0300

    INICIANDO COMUNICAÇÃO COM TELAS

--- src/main/java/scenebuilder/com/example/estoqueti/HelloApplication.java
+++ src/main/java/scenebuilder/com/example/estoqueti/HelloApplication.java
@@ -2,22 +2,25 @@ package scenebuilder.com.example.estoqueti;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    Stage janela;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        janela = stage;
+        Parent tela = FXMLLoader.load(getClass().getResource("login.fxml"));
+
+        Scene scene = new Scene(tela);
+        janela.setScene(scene);
+        janela.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+
 }
