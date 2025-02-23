a004f0092647037ce785f87e18d4f5ac66a8c919
IuriBreno <brenoiuri42@gmail.com>
2025-02-23 16:07:52 -0300

    FINALIZADO

--- src/main/java/scenebuilder/com/example/estoqueti/HelloApplication.java
+++ src/main/java/scenebuilder/com/example/estoqueti/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
+        //stage.getIcons().add(new Image(getClass().getResourceAsStream("/scenebuilder/com/example/estoqueti/resources/LOGO_VULCABRAS_V_cor.pngz")));
+
         janela = stage;
         Parent tela = FXMLLoader.load(getClass().getResource("login.fxml"));
 
